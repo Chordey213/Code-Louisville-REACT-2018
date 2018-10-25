fd5acdc95e061cb4e6324339074c53347e1cd626
Chordey213 <cpwalla026@gmail.com>
2018-10-24 20:26:09 -0400

    created header image element and added search box=shit

--- Steam Punk/steam-punk/src/App.js
+++ Steam Punk/steam-punk/src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import giraffe from './image.png';
 
 class App extends Component {
+constructor (props) {
+  super()
+  this.state = {value: ''};
+  this.search = this.search.bind(this); // bind the search field to the search text//
+  this.submit = this.submit.bind(this);
+}
+   search(event) {
+     this.setState({value: event.target.value});
+   };
+
+   submit(event) {
+     alert('FUCK YEAH ' + this.state.value)
+     event.preventDefault();
+   };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-header" >
+          <img src={giraffe} alt="logo"></img>
+        </div>
+        <div>
+          <input type='text' value={this.state.value} onChange={this.search}></input>
+          <input type='button' value='submit' onClick={this.submit}></input>
+        </div>
       </div>
     );
   }
