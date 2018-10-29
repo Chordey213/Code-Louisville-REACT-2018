827bf5bc0e44d7811820094a82c26aabfda876d9
Chordey213 <cpwalla026@gmail.com>
2018-10-28 22:02:36 -0400

    creating new app files, and prepairing to upload

---
+++ steamy/steamy/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    value: [
+
+    ]
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <body>
+        </body>
+      </div>
+    );
+  }
+}
+
+export default App;
