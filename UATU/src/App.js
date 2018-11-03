ec4c527f10f79ee378d245c64dc2c858d4ddf0ab
Chordey213 <cpwalla026@gmail.com>
2018-11-02 21:01:54 -0400

    updating, adding boxes to wirefram, prepairing components

--- UATU/src/App.js
+++ UATU/src/App.js
@@ -5,22 +5,23 @@ import './img/uatu1.jpg';
 class App extends Component {
   constructor(props) {
     super()
-    // state = [];
+
+
+  
 
   }
   render() {
-    // Left hand side of page is Uatu the watcher (picture) 
-    // Top of the page is the Marvel character lineup image
-    // main page content is a search bar, with a description of what you can do
-
     return (
       <table className="uatu">
         <tbody>
-          <tr className="top">
+          <tr className="top"> 
+              {/* // Left hand side of page is Uatu the watcher (picture) // */}
             <td rowSpan="2" className="one"><img className="img" src="https://via.placeholder.com/200x350.png?text=UATU+goes+here" alt="shit"></img></td>
+              {/* // Top of the page is the Marvel character lineup image // */}
             <td colSpan="2"><img className="img" src="https://via.placeholder.com/780x100.png?text=Marvel+Banner+goes+here" alt="shit"></img></td>
           </tr>
           <tr>
+              {/* // main page content is a search bar, with a description of what you can do // */}
             <td className="two"><img className="img" src="https://via.placeholder.com/730x400.png?text=SEARCH+goes+here" alt="shit"></img></td>
           </tr>
         </tbody>
